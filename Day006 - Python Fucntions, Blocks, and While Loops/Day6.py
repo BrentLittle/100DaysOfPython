6685df2d4ed58fb97f8252c4d73dec27a5d83653
Brent Littlefield <brentlittlefield4@gmail.com>
2021-05-12 19:45:40 -0400

    Day 6 Completed

---
+++ Day006 - Python Fucntions, Blocks, and While Loops/Day6.py
@@ -0,0 +1,16 @@
+### Python Functions ###
+def func():
+    print("Start")
+    print("End")
+func()
+
+
+
+
+### indentation ###
+# All lines of code that go within a function must be indented to the required indentation level
+
+
+### While Loops ###
+
+## ALL CODING FOR THIS SECTION WAS DONE ONLINE ##
