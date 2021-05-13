d680e2decff28880e478d40e2187a374d93edb60
Brent Littlefield <brentlittlefield4@gmail.com>
2021-05-12 20:36:16 -0400

    Day007 Start

---
+++ Day007 - Hangman/Day7.py
@@ -0,0 +1,11 @@
+### Project Day ###
+
+# The goal of today is to create the code version of Hangman
+
+# This will work the same way the regular game of hangman should work
+    # You guess a letter, if it is wrong a body part is added to the hanging man
+    # if it is a letter that is within the word to be guessed it is displayed to the player to give them more information as to what the hidden word is
+
+
+## hangmanwordgame.com
+
